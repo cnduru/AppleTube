d4adf25820e049ad27151d392f0b7e6d423797ef
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2016-03-30 20:23:43 +0200

    init

---
+++ AppleTube/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace AppleTube
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(12, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(756, 343);
+            this.panel1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(756, 343);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.FileDownload += new System.EventHandler(this.webBrowser1_FileDownload);
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            this.webBrowser1.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser1_Navigating);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(13, -8);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisconnect.TabIndex = 1;
+            this.buttonDisconnect.Text = "button1";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 376);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button buttonDisconnect;
+
+    }
+}
+
---
+++ AppleTube/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppleTube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //webBrowser1.Navigate("www.youtubeinmp3.com/fetch/?video=http://www.youtube.com/watch?v=i62Zjga8JOM");
+            webBrowser1.Navigate("youtube.com");
+        }
+
+        //long milliseconds;
+
+        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+
+        }
+
+        List<string> urls = new List<string>();
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            if(webBrowser1.Url != null && webBrowser1.Url.ToString().Contains("watch"))
+            {
+                string url = webBrowser1.Url.ToString();
+
+                // to avoid repeat loading of URLs
+                if (!urls.Contains(url))
+                {
+                    // thread parameterize this
+                    download("http://www.youtubeinmp3.com/fetch/?video=" + url);
+                    //sendPlayRequest("");
+                }
+
+                urls.Add(url);
+
+                webBrowser1.Navigate("http://www.youtube.com");
+            } 
+            
+        }
+
+        private void webBrowser1_FileDownload(object sender, EventArgs e)
+        {
+            // remove this
+        }
+
+        private void download(string url)
+        {
+            string fileName = url.Substring(url.IndexOf("v=") + 2) + ".mp3";
+
+            // download file
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            myHttpWebRequest.MaximumAutomaticRedirections = 10;
+            myHttpWebRequest.AllowAutoRedirect = true;
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+
+            using (var responseStream = myHttpWebResponse.GetResponseStream()) 
+            {
+                using (var fileStream = new FileStream(@"C:\Users\Avalon\Desktop\php\" + fileName, FileMode.Create)) 
+                {
+                    responseStream.CopyTo(fileStream);
+                }
+            }
+
+            sendPlayRequest("http://192.168.0.13:80/" + fileName, fileName);
+        }
+
+        TcpClient tc;
+
+        private void sendPlayRequest(string url, string filename)
+        {
+            try
+            {
+                tc = new TcpClient("192.168.0.18", 7000);
+
+                StreamWriter st = new StreamWriter(tc.GetStream());
+                StreamReader sr = new StreamReader(tc.GetStream());
+                string body = getPlayBody(url, filename);
+                st.Write(body);
+                st.Write("\n\n");
+                st.Flush();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private string getPlayBody(string url, string filename)
+        {
+            string req = String.Format("POST /play HTTP/1.1\n" +
+                   "User-Agent: MediaControl/1.0\n" +
+                   "Content-Type: text/parameters\n" +
+                   "Content-Length: {0}\n" +           
+                   "X-Apple-Session-ID: fb6d816a-a5ad-4e8f-8830-9642b6e6eb35\n\n", getPlayContent(url, filename).Length + 1);
+
+            return req + getPlayContent(url, filename);
+        }
+
+        private string getPlayContent(string url, string filename)
+        {
+            return String.Format("Content-Location: http://192.168.0.13:80/{0}\n" +
+                   "Start-Position: 0\n", filename);
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            tc.Close();
+        }
+                
+        
+    }
+}
